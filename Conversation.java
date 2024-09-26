1a62d8155dac80dd51b6c3195610d6febb204ce0
Amelia <aaquinn@smith.edu>
2024-09-26 17:17:49 -0400

    modified:   Conversation.java 	modified:   reflection.md 	modified:   rubric.md

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,80 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+/**
+ * <p> This class can execute a basic conversation with the Chatbot. </p>
+ * @author Amelia Quinn
+ * @version 1.0
+ */
 class Conversation {
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
-  }
-}
+  public static void main(String[] args) {
+    // first I create my scanner and my random instance
+    Scanner scanner = new Scanner(System.in);
+    Scanner scanner2 = new Scanner(System.in);
+    Random random = new Random();
+    List<String> transcript = new ArrayList<>();
+// This boolean is to keep track of whether we're going to mirror the user's response
+    boolean mirror = false;
+    // then I'm making a list of all the pronouns I'm going to check for 
+    List<String> pronounList = new ArrayList<>();
+    pronounList.add("I");
+    pronounList.add("me");
+    pronounList.add("am");
+    pronounList.add("you");
+    pronounList.add("my");
+    pronounList.add("your");
+    // Then I make a corresponding lists of all the replaecment pronouns
+    List<String> replacementList = new ArrayList<>();
+    replacementList.add("you");
+    replacementList.add("you");
+    replacementList.add("are");
+    replacementList.add("I");
+    replacementList.add("your");
+    replacementList.add("my");
+    // Then we ask the user how many rounds and store their response
+    System.out.println("How many rounds?");
+    int roundCount = scanner.nextInt();
+    // Then we begin the conversation and iterate as many times as the user tells us!
+    String hello = "Hello! What's on your mind?";
+    System.out.println(hello);
+    transcript.add(hello);
+    for (int i=1; i<= roundCount; i++) {
+      Scanner newScanner = new Scanner(System.in);
+      String userResponse = newScanner.nextLine();
+      transcript.add(userResponse);
+      for (int n=0; n <= 5; n++) {
+        String pronoun = pronounList.get(n);
+        String replacement = replacementList.get(n);
+        if (userResponse.contains(pronoun)) {
+          userResponse = userResponse.replaceAll(pronoun,replacement);
+          mirror = true;
+        } }
+      if (mirror == true) {
+      System.out.println(userResponse + "?"); 
+      transcript.add(userResponse + "?");
+    } else {
+        List<String> cannedResponses = new ArrayList<>();
+        cannedResponses.add("Interesting!");
+        cannedResponses.add("Mm-hm!");
+        cannedResponses.add("Uh-huh.");
+        int randomIdentifier = random.nextInt(cannedResponses.size());
+        String randomResponse = cannedResponses.get(randomIdentifier);
+        System.out.println(randomResponse);
+        transcript.add(randomResponse);
+        }
+        newScanner.close();
+       }
+       scanner.close();
+       scanner2.close();
+       System.out.println(transcript);
+      }
+      
+      }
+    
+
+
+
+  
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Alex Wood & Samantha Pincus - it was helpful to see how others attacked the problem & we helped each other with Java syntax!
 - Which session(s) of TA / office hours did you attend?
+Wednesday, September 25th - TA hours! I didn't really have any specific questions as it was just figuring out the syntax for me but it was good to be an environment where I knew there was help available!
 - What are your initial impressions of Java? 
+I am so used to Python -- I am not used at all to defining everything so explicitly. While it is a little bit of a headache when writing simpler programs like this one, I can definitely see how it would make things way way easier when coding bigger projects. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+On a grander scale, especially in a program that is heavily interactive, it is easier to think of code in an object-oriented manner as opposed to procedurally. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+Talking out my ideas, even to a person who is not involved in CS at all, is very helpful because sometimes articulating my ideas shows that they either do / don't make sense. I still haven't figured out all the intricacies -- particularly with the mirrored responses. I would tell someone taking this course in the future to collaborate!!! I mostly did this by myself but I think it would've been a lot easier and faster had I collaborated more with my peers instead of talking at the very end. 
